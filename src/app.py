2f0972a2ab2a420e4a04a2c386b5d22c2fb26c22
kiran566 <kiranbasava230@gmail.com>
2025-12-03 18:54:03 +0530

    streamlit

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ import os
 import gdown
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image
 
 MODEL_FILENAME = "best_model.h5"
 MODEL_DRIVE_URL = "https://drive.google.com/uc?id=1Q2YL3PLCuec5-PM_6iNNQyLe6_pCz6ai"
@@ -22,6 +23,7 @@ def load_cnn_model():
 model = load_cnn_model()
 
 def predict_image(img, img_size=(224, 224)):
+    img = img.convert("RGB")
     img = img.resize(img_size)
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
@@ -30,13 +32,16 @@ def predict_image(img, img_size=(224, 224)):
     return label, prob
 
 st.title("🩺 Pneumonia Detection from Chest X-ray")
-st.write("Upload a chest X-ray image to detect **Pneumonia** using your trained CNN model.")
+st.write("Upload a chest X-ray image to detect **Pneumonia**.")
 
 uploaded_file = st.file_uploader("Upload Chest X-ray Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img = load_img(uploaded_file)
+    img = Image.open(uploaded_file)
+
     st.image(img, caption="Uploaded Chest X-ray", use_column_width=True)
 
     if st.button("Predict"):
-        label, confidence = predict_image_
+        label, confidence = predict_image(img)
+        st.success(f"Prediction: {label}")
+        st.info(f"Confidence: {confidence:.4f}")
